86d31af8eee729ae214f9ee930ef2c8ceb720468
shibasky <sejalmestry953@student.sfit.ac.in>
2026-03-27 14:21:33 +0530

    day 1 question 1 reverse string keeping the position same

---
+++ reverse-1.js
@@ -0,0 +1,18 @@
+let str = 'hello world';
+
+function reverseString(){
+    let result = '';
+    for(let i = str.length-1; i >=0; i--){
+        result += str[i];
+    }
+    result = result.split(" ").reverse().join(" ");
+    return result;
+}
+
+console.log(reverseString());
+
+
+
+
+
+
