0018480cf4d30db6eb7d74dd3081ce17e6744c73
shibasky <sejalmestry953@student.sfit.ac.in>
2026-03-27 21:49:58 +0530

    day 1 question 3 palindrome

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+let str = "madam";
+
+function palindrome(){
+    let i = 0;
+    let j = str.length -1;
+
+    while(i<j){
+        if(str[i] !== str[j]){
+            return false 
+        }
+        i++;
+        j--;
+    }
+    return true; 
+}
+
+console.log(palindrome());
