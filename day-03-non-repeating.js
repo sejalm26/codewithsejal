96910b1b4d10aaaa37529e6540f99444fc45e129
shibasky <sejalmestry953@student.sfit.ac.in>
2026-03-29 22:27:06 +0530

    print non repeating character

---
+++ day-03-non-repeating.js
@@ -0,0 +1,22 @@
+let str = "aabbcd"
+
+function nonRepChar() {
+
+    for (let i = 0; i < str.length; i++) {
+        let count = 0;
+
+        for (let j = 0; j < str.length; j++) {
+            if (str[i] === str[j]) {
+                count++;
+            }
+        }
+        
+        if (count === 1) {
+            console.log(str[i]);
+            break;
+
+        }
+    }
+}
+
+nonRepChar();
